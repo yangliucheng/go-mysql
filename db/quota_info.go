d49888e5c948fb61502a0d1937e56eec70f7cf77
yangliucheng <yangliucheng_chn@163.com>
2016-12-12 23:21:53 -0800

    rm

--- db/quota_info.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type QuotaInfo struct {
-	MarathonName string
-	AppId        string
-	RuleType     string
-	MaxThreshold float64
-	MinThreshold float64
-}
--- db/scale_log.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type ScaleLog struct {
-	MarathonName string
-	AppId        string
-	Time         string
-	CountStatus  string
-	Event        string
-}
